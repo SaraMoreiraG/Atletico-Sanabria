30061b3b9e968df245adf7948f1defb714e8f935
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-27 18:15:14 +0200

    editing server

--- api/routes/clasificationdb.js
+++ api/routes/clasificationdb.js
@@ -1,97 +1,56 @@
 const express = require('express');
-const AWS = require('aws-sdk');
+const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb'); // Import the AWS SDK for JavaScript (v3)
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb'); // Utility functions for DynamoDB data
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const router = express.Router();
 
+const dynamodbClient = new DynamoDBClient({
+	region: 'us-east-1', // Set your desired AWS region
+	credentials: {
+	  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	},
+  });
 
-// Configure AWS SDK
-AWS.config.update({
-  region: 'us-east-1', // AWS region
-  accessKeyId: '', // IAM user access key
-  secretAccessKey: '', // IAM user secret key
-});
-
-const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 // Fetch all data and sort by 'position'
-router.get('/full', (req, res) => {
-	const params = {
-	  TableName: 'clasification', // DynamoDB table name
-	};
+router.get('/full', async (req, res) => {
+  const params = {
+    TableName: 'clasification', // DynamoDB table name
+  };
 
-	dynamodb.scan(params, (err, data) => {
-	  if (err) {
-		console.error(err);
-		res.status(500).send('Server Error');
-	  } else {
-		res.json(data.Items);
-	  }
-	});
-  });
+  try {
+    const command = new ScanCommand(params);
+    const data = await dynamodbClient.send(command);
+
+    const items = data.Items.map((item) => unmarshall(item));
+    res.json(items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
 // Fetch the first 5 items
-router.get('/onlyfive', (req, res) => {
-	const params = {
-	  TableName: 'clasification', // Specify your DynamoDB table name
-	  Limit: 5, // Limit the number of items to 5
-	};
+router.get('/onlyfive', async (req, res) => {
+  const params = {
+    TableName: 'clasification', // Specify your DynamoDB table name
+    Limit: 5, // Limit the number of items to 5
+  };
 
-	dynamodb.scan(params, (err, data) => {
-	  if (err) {
-		console.error(err);
-		res.status(500).send('Server Error');
-	  } else {
-		res.status(200).json(data.Items);
-	  }
-	});
-  });
+  try {
+    const command = new ScanCommand(params);
+    const data = await dynamodbClient.send(command);
 
-// Handle item updates
-router.put("/update/:itemId", (req, res) => {
-	const itemId = parseInt(req.params.itemId, 10); // Parse the ID as a number
-	console.log('Received request to update item with ID:', itemId);
-
-	const updatedData = req.body;
-	console.log("Updated data:", updatedData);
-
-	// Construct the update operation for DynamoDB
-	const updateExpression = [];
-	const expressionAttributeValues = {};
-
-	if (updatedData.name) {
-	  updateExpression.push("#name = :name");
-	  expressionAttributeValues[":name"] = updatedData.name;
-	}
-
-	// Add other fields you want to update in a similar manner
-
-	const params = {
-	  TableName: "clasification",
-	  Key: {
-		id: itemId, // ID is a number
-	  },
-	  UpdateExpression: "SET " + updateExpression.join(", "),
-	  ExpressionAttributeNames: {
-		"#name": "name",
-		// Add other attribute names you use in your updateExpression
-	  },
-	  ExpressionAttributeValues: expressionAttributeValues,
-	};
-
-	console.log("Update params:", params);
-
-	// Update the item in DynamoDB
-	dynamodb.update(params, (err, data) => {
-	  if (err) {
-		console.error("DynamoDB update error:", err);
-		res.status(500).send("Server Error");
-	  } else {
-		console.log("Item successfully updated in DynamoDB.");
-		res.sendStatus(200); // Successfully updated
-	  }
-	});
-  });
-
-
-
+    const items = data.Items.map((item) => unmarshall(item));
+    res.status(200).json(items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
