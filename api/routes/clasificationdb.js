786be1d1ac2f6e39ce3085ef51b3c248d1f62200
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-28 23:04:45 +0200

    All routes for clasificationdb

--- api/routes/clasificationdb.js
+++ api/routes/clasificationdb.js
@@ -1,55 +1,239 @@
-const express = require('express');
-const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb'); // Import the AWS SDK for JavaScript (v3)
-const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb'); // Utility functions for DynamoDB data
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const {
+  DynamoDBClient,
+  ScanCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
+  PutItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
 dotenv.config();
-
 const router = express.Router();
 
+router.use(cors());
+router.use(express.json());
+
 const dynamodbClient = new DynamoDBClient({
-	region: 'us-east-1', // Set your desired AWS region
-	credentials: {
-	  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	},
-  });
-
-
-// Fetch all data and sort by 'position'
-router.get('/full', async (req, res) => {
-  const params = {
-    TableName: 'clasification', // DynamoDB table name
-  };
-
-  try {
-    const command = new ScanCommand(params);
-    const data = await dynamodbClient.send(command);
-
-    const items = data.Items.map((item) => unmarshall(item));
-    res.json(items);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
-  }
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
-// Fetch the first 5 items
-router.get('/onlyfive', async (req, res) => {
-  const params = {
-    TableName: 'clasification', // Specify your DynamoDB table name
-    Limit: 5, // Limit the number of items to 5
+// GET route to retrieve all items from the table
+router.get("/full", (req, res) => {
+  const scanParams = {
+    TableName: "clasification",
   };
 
-  try {
-    const command = new ScanCommand(params);
-    const data = await dynamodbClient.send(command);
+  dynamodbClient
+    .send(new ScanCommand(scanParams))
+    .then((data) => {
+      // Process the items to convert attribute values to plain JavaScript types
+      const items = data.Items.map((item) => {
+        const unmarshalledItem = unmarshall(item);
+        // Convert attribute values to plain JavaScript types
+        Object.keys(unmarshalledItem).forEach((key) => {
+          const value = unmarshalledItem[key];
+          if (value && typeof value === "object" && value.hasOwnProperty("N")) {
+            unmarshalledItem[key] = parseInt(value.N);
+          } else if (value.hasOwnProperty("S")) {
+            unmarshalledItem[key] = value.S;
+          }
+        });
+        return unmarshalledItem;
+      });
 
-    const items = data.Items.map((item) => unmarshall(item));
-    res.status(200).json(items);
+      res.json(items);
+    })
+    .catch((err) => {
+      console.error("DynamoDB scan error:", err);
+      res.status(500).send("Server Error");
+    });
+});
+
+// GET route to retrieve the first 5 items from the table
+router.get("/onlyfive", (req, res) => {
+  const scanParams = {
+    TableName: "clasification",
+    Limit: 5, // Limit the result to the first 5 items
+  };
+
+  dynamodbClient
+    .send(new ScanCommand(scanParams))
+    .then((data) => {
+      // Process the items to convert attribute values to plain JavaScript types
+      const items = data.Items.map((item) => {
+        const unmarshalledItem = unmarshall(item);
+        // Convert attribute values to plain JavaScript types
+        Object.keys(unmarshalledItem).forEach((key) => {
+          const value = unmarshalledItem[key];
+          if (value && typeof value === "object" && value.hasOwnProperty("N")) {
+            unmarshalledItem[key] = parseInt(value.N);
+          } else if (value.hasOwnProperty("S")) {
+            unmarshalledItem[key] = value.S;
+          }
+        });
+        return unmarshalledItem;
+      });
+
+      res.json(items);
+    })
+    .catch((err) => {
+      console.error("DynamoDB scan error:", err);
+      res.status(500).send("Server Error");
+    });
+});
+
+// Update items
+router.put("/update/:itemId", (req, res) => {
+  const itemId = parseInt(req.params.itemId, 10);
+  const name = req.body.name;
+
+  if (!name) {
+    res.status(400).send("Name is missing in the request body");
+    return;
+  }
+
+  const updatedData = req.body;
+  const updateExpression = [];
+  const expressionAttributeNames = {};
+  const expressionAttributeValues = {};
+
+  // Check and update multiple attributes
+  if (typeof updatedData.pe === "number") {
+    updateExpression.push("#pe = :pe");
+    expressionAttributeNames["#pe"] = "pe";
+    expressionAttributeValues[":pe"] = { N: updatedData.pe.toString() };
+  }
+
+  if (typeof updatedData.pg === "number") {
+    updateExpression.push("#pg = :pg");
+    expressionAttributeNames["#pg"] = "pg";
+    expressionAttributeValues[":pg"] = { N: updatedData.pg.toString() };
+  }
+
+  if (typeof updatedData.pj === "number") {
+    updateExpression.push("#pj = :pj");
+    expressionAttributeNames["#pj"] = "pj";
+    expressionAttributeValues[":pj"] = { N: updatedData.pj.toString() };
+  }
+
+  if (typeof updatedData.pp === "number") {
+    updateExpression.push("#pp = :pp");
+    expressionAttributeNames["#pp"] = "pp";
+    expressionAttributeValues[":pp"] = { N: updatedData.pp.toString() };
+  }
+
+  if (typeof updatedData.pts === "number") {
+    updateExpression.push("#pts = :pts");
+    expressionAttributeNames["#pts"] = "pts";
+    expressionAttributeValues[":pts"] = { N: updatedData.pts.toString() };
+  }
+
+  const updateCommand = {
+    TableName: "clasification",
+    Key: {
+      id: { N: itemId.toString() },
+      name: { S: name },
+    },
+    UpdateExpression: "SET " + updateExpression.join(", "),
+    ExpressionAttributeNames: expressionAttributeNames,
+    ExpressionAttributeValues: marshall(expressionAttributeValues),
+  };
+
+  dynamodbClient
+    .send(new UpdateItemCommand(updateCommand))
+    .then((data) => {
+      console.log("Item successfully updated in DynamoDB.");
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error("DynamoDB update error:", err);
+      res.status(500).send("Server Error");
+    });
+});
+
+// Delete items
+router.delete("/delete/:itemId/:itemName", (req, res) => {
+  const itemId = parseInt(req.params.itemId, 10);
+  const itemName = req.params.itemName;
+
+  const deleteParams = {
+    TableName: "clasification",
+    Key: {
+      id: { N: itemId.toString() },
+      name: { S: itemName.toString() },
+    },
+  };
+
+  dynamodbClient
+    .send(new DeleteItemCommand(deleteParams))
+    .then(() => {
+      console.log("Item deleted successfully.");
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.error("DynamoDB delete error:", err);
+      res.status(500).send("Server Error");
+    });
+});
+
+// Create Item
+router.post("/add", async (req, res) => {
+  const newTeam = req.body;
+
+  if (
+    !newTeam ||
+    !newTeam.name ||
+    !newTeam.pj ||
+    !newTeam.pg ||
+    !newTeam.pe ||
+    !newTeam.pp ||
+    !newTeam.pts
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    // Generate a random unique ID
+    const uniqueId = Date.now() + Math.floor(Math.random() * 1000);
+
+    const newTeamParams = {
+      TableName: "clasification",
+      Item: {
+        id: { N: "1698524243177" },
+        name: { S: "dsa" },
+        pj: { N: "2" },
+        pg: { N: "2" },
+        pe: { N: "2" },
+        pp: { N: "2" },
+        pts: { N: "2" },
+      },
+    };
+
+    // Use DynamoDBClient to send the PutItemCommand
+    dynamodbClient
+      .send(new PutItemCommand(newTeamParams))
+      .then(() => {
+        res.status(201).json({
+          message: "New team created successfully",
+        });
+      })
+      .catch((err) => {
+        console.error("Error adding new team:", err);
+        res
+          .status(500)
+          .json({ error: "An error occurred while creating the team" });
+      });
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
+    console.error("Error adding item:", error);
+    res.status(500).json({ error: "Adding Error" });
   }
 });
 
--- src/management/Clasification/Clasification.js
+++ src/management/Clasification/Clasification.js
@@ -12,6 +12,86 @@ function Clasification() {
     rowIndex: null,
     columnIndex: null,
   });
+  const [addingTeam, setAddingTeam] = useState(false);
+  const [newTeam, setNewTeam] = useState({
+    name: "",
+    pj: "",
+    pg: "",
+    pe: "",
+    pp: "",
+    pts: "",
+  });
+
+  const handleInputChangeNewTeam = (event) => {
+    const { name, value } = event.target;
+    setNewTeam((prevTeam) => ({
+      ...prevTeam,
+      [name]: value,
+    }));
+  };
+
+  const handleAddTeam = () => {
+    // Generate a random ID using a timestamp and a random number
+    const randomId = Date.now() + Math.floor(Math.random() * 1000);
+
+    // Check if all required properties are defined
+    if (
+      newTeam &&
+      newTeam.name !== undefined &&
+      newTeam.pj !== undefined &&
+      newTeam.pg !== undefined &&
+      newTeam.pe !== undefined &&
+      newTeam.pp !== undefined &&
+      newTeam.pts !== undefined
+    ) {
+      // Prepare the data to send to the server, including the generated ID
+      const newTeamData = {
+        name: newTeam.name,
+        pj: parseInt(newTeam.pj),
+        pg: parseInt(newTeam.pg),
+        pe: parseInt(newTeam.pe),
+        pp: parseInt(newTeam.pp),
+        pts: parseInt(newTeam.pts),
+      };
+      console.log(newTeamData)
+      // Send a POST request to your server using fetch or Axios
+      fetch("http://localhost:3001/clasificationdb/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTeamData), // Convert data to JSON
+      })
+        .then((response) => {
+          if (response.status === 201) {
+            // Team added successfully
+            alert("Team added successfully.");
+            // Clear the form or update the UI as needed
+            setNewTeam({
+              id: "", // Clear the ID field
+              name: "",
+              pj: "",
+              pg: "",
+              pe: "",
+              pp: "",
+              pts: "",
+            });
+          } else {
+            // Handle any other response status codes or errors here
+            alert("Failed to add the team. Please try again.");
+          }
+        })
+        .catch((error) => {
+          console.error("An error occurred:", error);
+          // Handle the error as needed
+        });
+    } else {
+      // Handle the case where some properties are undefined
+      alert("Please fill in all the required fields.");
+    }
+  };
+
+
 
   useEffect(() => {
     // Define the URL to fetch data
@@ -28,6 +108,7 @@ function Clasification() {
       .then((data) => {
         data.sort((a, b) => b.pts - a.pts);
         setData(data);
+        console.log(data)
         setLoading(false);
       })
       .catch((err) => {
@@ -48,13 +129,11 @@ function Clasification() {
     return <div>Error: {error.message}</div>;
   }
 
-  const handleEditClick = (rowIndex, itemId) => {
-    // Set the edit state for the clicked row
+  const handleEditClick = (rowIndex) => {
     setEditState({ rowIndex, columnIndex: null });
   };
 
   const handleInputChange = (rowIndex, columnName, value) => {
-    // Update the data in a controlled manner when inputs change
     const updatedData = [...data];
     updatedData[rowIndex][columnName] = value;
     setData(updatedData);
@@ -62,15 +141,23 @@ function Clasification() {
 
   const handleSaveClick = (item) => {
     setEditState({ rowIndex: null, columnIndex: null });
-console.log(item)
-    // Make an API call to update the item in the backend
+
+    const updatedFields = {
+      pe: item.pe,
+      pg: item.pg,
+      pj: item.pj,
+      pp: item.pp,
+      pts: item.pts,
+      name: item.name,
+    };
+
     const apiUrl = `http://localhost:3001/clasificationdb/update/${item.id}`;
     fetch(apiUrl, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(item),
+      body: JSON.stringify(updatedFields), // Send only the fields you want to update
     })
       .then((response) => {
         if (!response.ok) {
@@ -89,11 +176,33 @@ console.log(item)
     return editState.rowIndex === rowIndex;
   };
 
+  const handleDeleteClick = (itemId, itemName) => {
+    fetch(`http://localhost:3001/clasificationdb/delete/${itemId}/${itemName}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          // Item deleted successfully
+          alert("Item deleted successfully.");
+        } else {
+          // Failed to delete the item
+          alert("Failed to delete the item. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+  };
+
   let position = 1;
 
   return (
     <div className="clasification col-4 p-3 my-5">
       <h3 className="mb-3">Clasificación</h3>
+      <button onClick={() => setAddingTeam(true)}>Añadir equipo</button>
       <table className="clasification-table">
         <thead>
           <tr className="text-center">
@@ -122,24 +231,22 @@ console.log(item)
                   className="me-2"
                   alt={item.name}
                 />
-                {isRowInEditState(rowIndex) ? (
-                  <input
-                    type="text"
-                    value={item.name}
-                    onChange={(e) =>
-                      handleInputChange(rowIndex, "name", e.target.value)
-                    }
-                  />
-                ) : (
-                  item.name
-                )}
+                {item.name}
               </td>
               <td>
                 {isRowInEditState(rowIndex) ? (
                   <input
                     type="text"
-					value={item.pj.toString()}
-					onChange={(e) => handleInputChange(rowIndex, 'pj', parseInt(e.target.value, 10))}
+                    value={item.pj}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        handleInputChange(rowIndex, "pj", 0);
+                      } else if (/^\d*$/.test(value)) {
+                        handleInputChange(rowIndex, "pj", parseInt(value, 10));
+                      } else {
+                      }
+                    }}
                   />
                 ) : (
                   item.pj
@@ -150,9 +257,15 @@ console.log(item)
                   <input
                     type="text"
                     value={item.pg}
-                    onChange={(e) =>
-                      handleInputChange(rowIndex, "pg", e.target.value)
-                    }
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        handleInputChange(rowIndex, "pg", 0);
+                      } else if (/^\d*$/.test(value)) {
+                        handleInputChange(rowIndex, "pg", parseInt(value, 10));
+                      } else {
+                      }
+                    }}
                   />
                 ) : (
                   item.pg
@@ -163,9 +276,15 @@ console.log(item)
                   <input
                     type="text"
                     value={item.pe}
-                    onChange={(e) =>
-                      handleInputChange(rowIndex, "pe", e.target.value)
-                    }
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        handleInputChange(rowIndex, "pe", 0);
+                      } else if (/^\d*$/.test(value)) {
+                        handleInputChange(rowIndex, "pe", parseInt(value, 10));
+                      } else {
+                      }
+                    }}
                   />
                 ) : (
                   item.pe
@@ -176,9 +295,15 @@ console.log(item)
                   <input
                     type="text"
                     value={item.pp}
-                    onChange={(e) =>
-                      handleInputChange(rowIndex, "pp", e.target.value)
-                    }
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        handleInputChange(rowIndex, "pp", 0);
+                      } else if (/^\d*$/.test(value)) {
+                        handleInputChange(rowIndex, "pp", parseInt(value, 10));
+                      } else {
+                      }
+                    }}
                   />
                 ) : (
                   item.pp
@@ -189,9 +314,15 @@ console.log(item)
                   <input
                     type="text"
                     value={item.pts}
-                    onChange={(e) =>
-                      handleInputChange(rowIndex, "pts", e.target.value)
-                    }
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        handleInputChange(rowIndex, "pts", 0);
+                      } else if (/^\d*$/.test(value)) {
+                        handleInputChange(rowIndex, "pts", parseInt(value, 10));
+                      } else {
+                      }
+                    }}
                   />
                 ) : (
                   item.pts
@@ -199,19 +330,82 @@ console.log(item)
               </td>
               <td>
                 {isRowInEditState(rowIndex) ? (
-                  <button
-                    onClick={() => handleSaveClick(item)}
-                  >
-                    Save
-                  </button>
+                  <button onClick={() => handleSaveClick(item)}>Save</button>
                 ) : (
                   <button onClick={() => handleEditClick(rowIndex, item.id)}>
                     Edit
                   </button>
                 )}
               </td>
+              <td>
+                <button onClick={() => handleDeleteClick(item.id, item.name)}>
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
+          {addingTeam && (
+    <tr>
+    <td></td>
+    <td>
+      <input
+        type="text"
+        name="name"
+        placeholder="Nombre del equipo"
+        value={newTeam.name}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        name="pj"
+        placeholder="PJ"
+        value={newTeam.pj}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        name="pg"
+        placeholder="PG"
+        value={newTeam.pg}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        name="pe"
+        placeholder="PE"
+        value={newTeam.pe}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        name="pp"
+        placeholder="PP"
+        value={newTeam.pp}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <input
+        type="number"
+        name="pts"
+        placeholder="Pts"
+        value={newTeam.pts}
+        onChange={handleInputChangeNewTeam}
+      />
+    </td>
+    <td>
+      <button onClick={handleAddTeam}>Añadir</button>
+    </td>
+  </tr>
+          )}
         </tbody>
       </table>
     </div>
@@ -220,7 +414,8 @@ console.log(item)
 
 export default Clasification;
 
-          {/* {data.map((item, index ) => (
+{
+  /* {data.map((item, index ) => (
 		<tr key={index} className="text-center">
             <td className="py-2">{position++}.</td>
             <td className="text-start">
@@ -233,4 +428,5 @@ export default Clasification;
             <td>{item.pp}</td>
             <td>{item.pts}</td>
           </tr>
-        ))} */}
+        ))} */
+}
