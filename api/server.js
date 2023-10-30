20bf1c21500b169395c1293714102743fd014625
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-30 22:48:27 +0100

    preparing for deploy

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
