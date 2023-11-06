ca9672be661f441dcaa8acfe4c496e614ca1d0c9
SaraMoreiraG <sara_moreira@hotmail.es>
2023-11-06 23:08:41 +0100

    creating scroll clasification

--- src/components/PointTable/PointTable.js
+++ src/components/PointTable/PointTable.js
@@ -9,7 +9,7 @@ function PointTable() {
 
   useEffect(() => {
     // Define the URL to fetch data
-	const apiUrl = process.env.REACT_APP_API_URL + "/clasificationdb/onlyfive";
+	const apiUrl = process.env.REACT_APP_API_URL + "/clasificationdb/full";
 
     // Make a GET request to the API
     fetch(apiUrl)
@@ -43,46 +43,52 @@ function PointTable() {
   }
   let position = 1;
   return (
-    <div className="col-lg-4 col-md-12 px-2 d-flex align-items-start">
-      <div className="clasification col-12 p-3">
-        <h3 className="mb-3">Clasificación</h3>
-        <table className="clasification-table">
-          <thead>
-            <tr className="text-center">
-              <th className="col-1 p-2">Pos</th>
-              <th className="col-6 text-start">Equipo</th>
-              <th className="col-1">PJ</th>
-              <th className="col-1">PG</th>
-              <th className="col-1">PE</th>
-              <th className="col-1">PP</th>
-              <th className="col-1">Pts</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index} className="text-center">
-                <td className="py-2">{position++}.</td>
-                <td className="text-start">
-                  <img
-                    src={`https://images-atletico-sanabria.s3.amazonaws.com/logos/${item.shortName}.png`}
-                    height="25px"
-                    className="me-2"
-                    alt={`logo ${item.name}`}
-                  />
-
-                  {item.name}
-                </td>
-                <td>{item.pj}</td>
-                <td>{item.pg}</td>
-                <td>{item.pe}</td>
-                <td>{item.pp}</td>
-                <td>{item.pts}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+<div className="col-lg-4 col-md-12 px-2 d-flex align-items-start">
+  <div className="clasification col-12 p-3">
+    <h3 className="mb-3">Clasificación</h3>
+    <div className="table-container">
+      <table className="clasification-table">
+        <thead>
+          <tr className="text-center">
+            <th className="col-1 p-2">Pos</th>
+            <th className="col-6 text-start">Equipo</th>
+            <th className="col-1">PJ</th>
+            <th className="col-1">PG</th>
+            <th className="col-1">PE</th>
+            <th className="col-1">PP</th>
+            <th className="col-1">Pts</th>
+          </tr>
+        </thead>
+      </table>
     </div>
+    <div className="table-container">
+      <table className="clasification-table">
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className="text-center">
+              <td className="col-1 py-2">{position++}.</td>
+              <td className="col-6 text-start">
+                <img
+                  src={`https://images-atletico-sanabria.s3.amazonaws.com/logos/${item.shortName}.png`}
+                  height="25px"
+                  className="me-2"
+                  alt={`logo ${item.name}`}
+                />
+                {item.name}
+              </td>
+              <td className="col-1">{item.pj}</td>
+              <td className="col-1">{item.pg}</td>
+              <td className="col-1">{item.pe}</td>
+              <td className="col-1">{item.pp}</td>
+              <td className="col-1">{item.pts}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
   );
 }
 
