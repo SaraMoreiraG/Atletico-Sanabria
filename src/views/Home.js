5f8ae8dc98e468c463cc8dd6f1b8407cbf8af54a
SaraMoreiraG <sara_moreira@hotmail.es>
2023-11-06 20:01:36 +0100

    navbar dropdown

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { scrollToSection } from "../utils/scrollUtils";
 
 import SimpleSlider from "../Slider";
