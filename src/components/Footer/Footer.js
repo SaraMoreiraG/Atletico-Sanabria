3d02a579e6f2ac9bffbba5b6110f386c47dc5203
SaraMoreiraG <sara_moreira@hotmail.es>
2023-11-07 03:09:05 +0100

    defining variables

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./Footer.css";
 import logo from "../../assets/images/atletico-sanabria-transparent.png";
-import { fetchInstagramSecretValue } from "../../utils/fetchInstagramSecretValue";
 
 function Footer() {
   const [instagramPhotos, setInstagramPhotos] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const secret = await fetchInstagramSecretValue();
-        // Use the secret data in your app
-        const instagramApiUrl = `https://graph.instagram.com/v12.0/me/media?fields=id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${secret}`;
-        fetch(instagramApiUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            setInstagramPhotos(data.data); // Assuming the photos are in the 'data' field
-          })
-          .catch((error) => {
-            console.error("Error fetching Instagram photos: ", error);
-          });
-        console.log(secret);
-      } catch (error) {
-        console.error("Error fetching secret:", error);
-      }
-    }
+    // Your Instagram API endpoint
+    const instagramApiUrl = `https://graph.instagram.com/v12.0/me/media?fields=id,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`;
 
-    fetchData();
+    fetch(instagramApiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setInstagramPhotos(data.data); // Assuming the photos are in the 'data' field
+      })
+      .catch((error) => {
+        console.error("Error fetching Instagram photos: ", error);
+      });
   }, []);
-
   return (
     <>
       <div id="footer">
@@ -90,7 +79,7 @@ function Footer() {
               <h2 className="blue">Instagram</h2>
             </div>
             <div className="row instagram-photos g-2">
-              {instagramPhotos.map((photo) => (
+              {instagramPhotos && instagramPhotos.map((photo) => (
                 <div key={photo.id} className="col-4">
                   <a
                     href={photo.permalink}
